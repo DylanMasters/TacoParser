a489a21816014ed36d57720e2d66705d4fca0e75
Dylan Masters <Dy1anmasters93@aol.com>
2020-02-20 18:28:44 -0600

    Completed Taco parser

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -9,10 +9,32 @@
         
         public ITrackable Parse(string line)
         {
-            logger.LogInfo("Begin parsing");
+            if (line == null)
+            {
+                return null;
+            }
 
+            TacoBell restaurant = new TacoBell();
+            var cells = line.Split(',');
             // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            if (cells.Length < 3)
+            {
+                logger.LogError("Not enough values to parse", null);
+                return null;
+            }
+            // TODO Implement
+
+            Point coordinates = new Point();
+            coordinates.Latitude = double.Parse(cells[0]);
+            coordinates.Longitude = double.Parse(cells[1]);
+            restaurant.Location = coordinates;
+            restaurant.Name = cells[2];
+            return restaurant;
         }
+    }
+    public class TacoBell : ITrackable
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
     }
 }
